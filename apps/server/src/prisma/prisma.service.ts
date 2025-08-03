64ad276fe7866471c63d25c4295f1627cb226aaf
bossnare <razafimangagervaischristo@gmail.com>
2025-08-03 21:49:01 +0300

    server:fix: add bunx prisma generate before build this app on Dockerfile

--- apps/server/src/prisma/prisma.service.ts
+++ apps/server/src/prisma/prisma.service.ts
@@ -8,15 +8,20 @@ export class PrismaService
 {
   async onModuleInit() {
     try {
+      if (!process.env.DATABASE_URL) {
+        throw new Error('DATABASE_URL environment variable is not set 📛');
+      }
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-call
       await this.$connect();
-      console.log('Connected to PostgreSQL via Prisma');
+      console.log('Connected to PostgreSQL via Prisma ✅');
     } catch (err) {
-      console.error('Failed to connect to PostgreSQL', err);
+      console.error('Failed to connect to PostgreSQL ❌📛', err);
     }
   }
 
   async onModuleDestroy() {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
     await this.$disconnect();
-    console.log('Disconnected from PostgreSQL');
+    console.log('Disconnected from PostgreSQL ♾️');
   }
 }
