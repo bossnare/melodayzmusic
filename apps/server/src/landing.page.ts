b073650b366acd6706e40f847843de955699123b
bossnare <razafimangagervaischristo@gmail.com>
2025-08-03 23:54:38 +0300

    server: Re-copy the rest of app on Dockerfile before pnpm build

--- apps/server/src/landing.page.ts
+++ apps/server/src/landing.page.ts
@@ -44,7 +44,7 @@ export const landingPage = `
     <div id="container">
     <h1 style="margin-top: 0; padding: 0">Welcome to MelodayzMusic API</h1>
     <p>
-        <a style="all: unset; color: #00BFFF" target="_blank" href=${apiDocs} >API Docs</a>
+        <a style="all: unset; color: #00BFFF" target="_blank" href=${apiDocs} >MelodayzMusic</a>
         is a web application designed to provide an engaging and
         seamless music experience for users.
     </p>
