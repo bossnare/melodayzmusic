b767059de51f65f97d70c3d31140d50189be6eb2
bossnare <razafimangagervaischristo@gmail.com>
2025-08-03 21:11:00 +0300

    server:fix: PrismaClient

--- apps/server/src/prisma/prisma.service.ts
+++ apps/server/src/prisma/prisma.service.ts
@@ -1,8 +1,11 @@
-import { PrismaClient } from 'generated/prisma';
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient {
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
   async onModuleInit() {
     try {
       await this.$connect();
