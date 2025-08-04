9518307bfb4ac85f7e552c255a31ad8e99459a0f
bossnare <razafimangagervaischristo@gmail.com>
2025-08-04 10:43:01 +0300

    add: prisma folder

--- apps/server/src/prisma/prisma.service.ts
+++ apps/server/src/prisma/prisma.service.ts
@@ -1,17 +1,17 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService
   extends PrismaClient
-  implements OnModuleInit
+  implements OnModuleInit, OnModuleDestroy
 {
   async onModuleInit() {
     try {
+      // Ensure env var don't exists
       if (!process.env.DATABASE_URL) {
         throw new Error('DATABASE_URL environment variable is not set 📛');
       }
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-call
       await this.$connect();
       console.log('Connected to PostgreSQL via Prisma ✅');
     } catch (err) {
@@ -20,7 +20,6 @@ export class PrismaService
   }
 
   async onModuleDestroy() {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
     await this.$disconnect();
     console.log('Disconnected from PostgreSQL ♾️');
   }
