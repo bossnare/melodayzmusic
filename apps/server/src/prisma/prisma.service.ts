b9412f2d3cff0112080a285958c311d5d7114ccc
bossnare <razafimangagervaischristo@gmail.com>
2025-08-05 09:17:25 +0300

    server: organize import

--- apps/server/src/prisma/prisma.service.ts
+++ apps/server/src/prisma/prisma.service.ts
@@ -1,5 +1,5 @@
-import { PrismaClient } from '../generated/prisma/client.js';
 import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '../generated/prisma/client.js';
 
 @Injectable()
 export class PrismaService
