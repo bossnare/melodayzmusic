4e79cba1446d106098ad126c3f34c6367b136630
bossnare <razafimangagervaischristo@gmail.com>
2025-08-05 17:56:45 +0300

    server:module: add module, controller, service user & song

---
+++ apps/server/src/song/song.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { SongController } from './song.controller.js';
+import { SongService } from './song.service.js';
+
+@Module({
+  controllers: [SongController],
+  providers: [SongService],
+})
+export class SongModule {}
