48a628c91221a3b0c64c657925af07db5aed7c19
bossnare <razafimangagervaischristo@gmail.com>
2025-08-07 16:48:39 +0300

    server: add JWT strategy

--- apps/server/src/auth/jwt/jwt.strategy.ts
+++ apps/server/src/auth/jwt/jwt.strategy.ts
@@ -1,13 +1,15 @@
-import { JwtPayload } from './../../types/auth/jwt-payload.interface.js';
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { JwtPayload } from './../../types/auth/jwt-payload.interface.js';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor() {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      passReqToCallback: true,
       secretOrKey: process.env.JWT_SECRET,
     });
   }
--- apps/server/src/prisma/prisma.module.ts
+++ apps/server/src/prisma/prisma.module.ts
@@ -1,7 +1,7 @@
 import { Global, Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service.js';
 
-@Global() // Available amin'ny module rehetra ilay module "PrismaModule" ity
+@Global() // Available amin'ny service rehetra ilay module "PrismaModule" ity
 @Module({
   providers: [PrismaService],
   exports: [PrismaService], // atao export: mba ho azo import manerana app
