aa288fcf253c0d5d9f3a5e6a911b991170afb0f0
bossnare <razafimangagervaischristo@gmail.com>
2025-08-07 17:02:16 +0300

    server: resolve conflict on api/auth branch

--- apps/server/src/user/dto/base-user.dto.ts
+++ apps/server/src/user/dto/base-user.dto.ts
@@ -1,4 +1,4 @@
-import { IsEmail, IsString } from 'class-validator';
+import { IsEmail, IsString, MinLength } from 'class-validator';
 
 export class BaseUserDto {
   @IsEmail()
@@ -8,5 +8,6 @@ export class BaseUserDto {
   name: string;
 
   @IsString()
+  @MinLength(6)
   password: string;
 }
