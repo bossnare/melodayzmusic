7f3ca0421e4800b4ec89e4316ff60e1a08dce932
bossnare <razafimangagervaischristo@gmail.com>
2025-08-08 10:58:18 +0300

    server:chore(auth): fix JwtStrategy class and add Joi for env validation

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -6,13 +6,20 @@ import { AuthModule } from './auth/auth.module.js';
 import { PrismaModule } from './prisma/prisma.module.js';
 import { SongModule } from './song/song.module.js';
 import { UserModule } from './user/user.module.js';
+import Joi from 'joi';
 
 @Module({
   imports: [
-    // global env file
     ConfigModule.forRoot({
-      isGlobal: true,
+      isGlobal: true, // global env file
       envFilePath: [`.env.${process.env.NODE_ENV || 'development'}`, `.env`], // '.env' fallback
+      // jwt_secret is required
+      validationSchema: Joi.object({
+        JWT_SECRET: Joi.string().required(),
+        NODE_ENV: Joi.string()
+          .valid('development', 'production', 'test')
+          .default('development'),
+      }),
     }),
     PrismaModule,
     UserModule,
--- apps/server/src/auth/jwt/jwt.strategy.ts
+++ apps/server/src/auth/jwt/jwt.strategy.ts
@@ -1,20 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { JwtPayload } from './../../types/auth/jwt-payload.interface.js';
 
 @Injectable()
-export class JwtStrategy extends PassportStrategy(Strategy) {
-  // constructor() {
-  //   super({
-  //     jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-  //     ignoreExpiration: false,
-  //     passReqToCallback: true,
-  //     secretOrKey: process.env.JWT_SECRET,
-  //   });
-  // }
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor(private configService: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: configService.get<string>('JWT_SECRET')!,
+    });
+  }
 
-  async validate(payload: JwtPayload) {
+  validate(payload: JwtPayload) {
     return {
       userId: payload.sub,
       email: payload.email,
