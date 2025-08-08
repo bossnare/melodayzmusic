db646ae939552e3bd60c13269901be4628bf59d5
bossnare <razafimangagervaischristo@gmail.com>
2025-08-08 23:47:03 +0300

    server:feat:auth: configure Global Guards, and decorator Public for any public route

--- apps/server/src/auth/auth.module.ts
+++ apps/server/src/auth/auth.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from './auth.controller.js';
 import { AuthService } from './auth.service.js';
-import { JwtStrategy } from './jwt/jwt.strategy.js';
+import { JwtStrategy } from './strategies/jwt.strategy.js';
 
 @Module({
   imports: [JwtModule.register({})],
--- apps/server/src/auth/strategies/jwt.strategy.ts
+++ apps/server/src/auth/strategies/jwt.strategy.ts
@@ -2,10 +2,10 @@ import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
-import { JwtPayload } from './../../types/auth/jwt-payload.interface.js';
+import { JwtPayload } from '../../types/auth/jwt-payload.interface.js';
 
 @Injectable()
-export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(private configService: ConfigService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
