f04c5fc1e90d83a31c4d4f4cf2085088ae117d0f
bossnare <razafimangagervaischristo@gmail.com>
2025-08-11 22:20:49 +0300

    client: add speed insights next, used it

--- apps/client/src/app/layout.tsx
+++ apps/client/src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import ReactQueryProvider from '@/libs/react-query/ReactQueryProvider';
 import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -32,6 +33,7 @@ export default function RootLayout({
       >
         <ReactQueryProvider>{children}</ReactQueryProvider>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
