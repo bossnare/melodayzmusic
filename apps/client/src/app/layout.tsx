93eaa0cd3a8d1fceab57aacaf29598264e07cc80
bossnare <razafimangagervaischristo@gmail.com>
2025-08-11 15:52:12 +0300

    client: use web analytics on main project, and ESNext on tsconfig

--- apps/client/src/app/layout.tsx
+++ apps/client/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import ReactQueryProvider from '@/libs/react-query/ReactQueryProvider';
+import { Analytics } from '@vercel/analytics/next';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -30,6 +31,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <ReactQueryProvider>{children}</ReactQueryProvider>
+        <Analytics />
       </body>
     </html>
   );
