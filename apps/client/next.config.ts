cbc8942c1ed9472390497ced2a0037f5aba6bd1d
bossnare <razafimangagervaischristo@gmail.com>
2025-08-13 09:21:38 +0300

    client: add pwa support and base  config on next.config.ts

--- apps/client/next.config.ts
+++ apps/client/next.config.ts
@@ -1,10 +1,20 @@
-import type { NextConfig } from "next";
+import createNextPWA from 'next-pwa';
+import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {
+// PWA config
+const withPWA = createNextPWA({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+});
+
+const nextConfig = {
   /* config options here */
   eslint: {
     ignoreDuringBuilds: true, // Atsahatra tanteraka ny lint amin'ny build
   },
-};
+  swcManify: true,
+} satisfies NextConfig; // ** type-safe with TS
 
-export default nextConfig;
+export default withPWA(nextConfig);
