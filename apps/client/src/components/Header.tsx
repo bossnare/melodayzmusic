5090a3b8705d11db47364bf72b6cfd18885fd1ce
Christo Razafimanga <144788145+bossnare@users.noreply.github.com>
2025-08-20 23:10:11 +0300

    Update Header.tsx: px-1 to px-2

--- apps/client/src/components/Header.tsx
+++ apps/client/src/components/Header.tsx
@@ -2,8 +2,9 @@ import { NavBar } from './Navbar';
 
 export const Header = () => {
   return (
-    <header className="sticky top-0 left-0 w-full px-1 py-2 border-b sm:px-6 z-7 border-b-gray-200 dark:border-b-gray-800 md:border-0 bg-gray-950">
+    <header className="sticky top-0 left-0 w-full px-2 py-2 border-b sm:px-6 z-7 border-b-gray-200 dark:border-b-gray-800 md:border-0 bg-gray-950">
       <NavBar />
     </header>
   );
 };
+
