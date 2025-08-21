7de5ca6d56519077887350da86f247e550620485
Christo Razafimanga <144788145+bossnare@users.noreply.github.com>
2025-08-21 23:52:52 +0300

    Update NavBottom.tsx: add transition on tab

--- apps/client/src/components/NavBottom.tsx
+++ apps/client/src/components/NavBottom.tsx
@@ -36,7 +36,7 @@ const NavTab = ({ href, icon, label }: Tab) => {
           pathname === href
             ? 'font-medium text-accent-foreground'
             : 'font-normal text-muted-foreground hover:text-foreground'
-        } select-none flex flex-col px-1 items-center justify-center gap-1 md:gap-2 md:flex-row`}
+        } select-none transition-transform duration-200 ease-in-out flex flex-col px-1 items-center justify-center gap-1 md:gap-2 md:flex-row`}
       >
         <span className={`${pathname === href ? '*:fill-accent-foreground' : ''}`}>
           {isLoading ? <LoaderCircle className="animate-spin" /> : icon}
@@ -111,6 +111,7 @@ export const NavBottom = () => {
     </nav>
   );
 };
+
 
 
 
