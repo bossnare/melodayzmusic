a0ca4bb4078f0cb9a7035cd6599d894b2ee8d7bb
Christo Razafimanga <144788145+bossnare@users.noreply.github.com>
2025-08-24 00:22:25 +0300

    Update NavBottom.tsx: update width on Plus button

--- apps/client/src/components/NavBottom.tsx
+++ apps/client/src/components/NavBottom.tsx
@@ -83,7 +83,7 @@ export const NavBottom = () => {
         tab.label === 'button' ? (
           <div
             key={tab.id}
-            className="flex flex-col items-center justify-center px-4 max-w-[10%]"
+            className="flex flex-col items-center justify-center px-4 max-w-[12%]"
           >
             <MotionButton
               size="icon"
@@ -110,3 +110,4 @@ export const NavBottom = () => {
     </div>
   );
 };
+
