83dccc1f2bb1f34902076ea5bded2086e245da25
Christo Razafimanga <144788145+bossnare@users.noreply.github.com>
2025-08-25 20:00:49 +0300

    Update SoftFade.tsx: update width for softFade on mobile

--- apps/client/src/components/songs/SoftFade.tsx
+++ apps/client/src/components/songs/SoftFade.tsx
@@ -2,9 +2,9 @@ const SoftFade = () => {
   return (
     <>
       {/* left overlay */}
-      <div className="absolute inset-y-0 left-0 w-8 pointer-events-none sm:w-12 bg-gradient-to-r from-gray-950/60 to-transparent z-2"></div>
+      <div className="absolute inset-y-0 left-0 w-10 pointer-events-none sm:w-12 bg-gradient-to-r from-gray-950/60 to-transparent z-2"></div>
       {/* right overlay */}
-      <div className="absolute inset-y-0 right-0 w-8 pointer-events-none sm:w-12 bg-gradient-to-l from-gray-950/60 to-transparent z-2"></div>
+      <div className="absolute inset-y-0 right-0 w-10 pointer-events-none sm:w-12 bg-gradient-to-l from-gray-950/60 to-transparent z-2"></div>
     </>
   );
 };
