4124da6883f9b95d573857992fc0c1b72ceb311a
Christo Razafimanga <144788145+bossnare@users.noreply.github.com>
2025-08-27 23:26:05 +0300

    Update layout.tsx: make overlay unclickable

--- apps/client/src/app/layout.tsx
+++ apps/client/src/app/layout.tsx
@@ -56,7 +56,7 @@ export default function RootLayout({
         className={`${inter.variable} ${poppins.variable} ${geistSans.variable} ${geistMono.variable} antialiased relative dark:bg-background`}
       >
         {/* ambiance overlay */}
-        <div className="absolute inset-0 z-1 bg-gradient-to-br from-[#8a2be2]/1 to-[#00D4FF]/1"></div>
+        <div className="absolute inset-0 z-1 pointer-events-none bg-gradient-to-br from-[#8a2be2]/1 to-[#00D4FF]/1"></div>
         <ThemeProvider attribute="class" defaultTheme="dark">
           <ReactQueryProvider>{children}</ReactQueryProvider>
           <Analytics />
@@ -66,3 +66,4 @@ export default function RootLayout({
     </html>
   );
 }
+
