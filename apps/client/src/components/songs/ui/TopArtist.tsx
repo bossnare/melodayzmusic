03aa3ccc2fa23ebba22573ff53f34f7e5e4d81ad
Christo Razafimanga <144788145+bossnare@users.noreply.github.com>
2025-08-28 01:09:51 +0300

    Update TopArtist.tsx: BG color

--- apps/client/src/components/songs/ui/TopArtist.tsx
+++ apps/client/src/components/songs/ui/TopArtist.tsx
@@ -7,7 +7,7 @@ const TopArtist = ({ children }: TopArtistProps) => {
       <div
         className="h-20 flex-nowrap
              flex items-center *:size-16 md:*:size-18 lg:*:size-22
-          *:bg-gray-100 dark:*:bg-skeleton gap-4 *:rounded-full"
+          *:bg-gray-100 dark:*:bg-primary-foreground gap-4 *:rounded-full"
       >
         {children}
       </div>
