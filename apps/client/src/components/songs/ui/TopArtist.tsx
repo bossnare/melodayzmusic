4f9b0cf2f3c72b4ab67858718890f1239dc3a6b7
Christo Razafimanga <144788145+bossnare@users.noreply.github.com>
2025-08-28 22:52:20 +0300

    Update TopArtist.tsx:UI: update grid on mobile

--- apps/client/src/components/songs/ui/TopArtist.tsx
+++ apps/client/src/components/songs/ui/TopArtist.tsx
@@ -11,7 +11,7 @@ const TopArtist = ({ children }: TopArtistProps) => {
         ref={emblaRef}
         className="overflow-hidden overflow-x-auto scroll-smooth scrollbar-none">
         <div
-          className="grid grid-flow-col auto-cols-[calc(100vw/4)] sm:auto-cols-[calc(100vw/8)] 
+          className="grid grid-flow-col auto-cols-[calc(100vw/5)] sm:auto-cols-[calc(100vw/8)] 
         lg:auto-cols-[calc(100vw/12)] gap-4 lg:gap-5"
         >
           {children}
