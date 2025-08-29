df8e67b024c805e5b21cbb2fa7fec3caec87c152
bossnare <razafimangagervaischristo@gmail.com>
2025-08-29 11:42:59 +0300

    client:ui: separate Vibe, Album card, reduce opacity on text-section

--- apps/client/src/components/songs/ui/SongCard.tsx
+++ apps/client/src/components/songs/ui/SongCard.tsx
@@ -18,7 +18,8 @@ import {
   CardTitle,
 } from '@/components/ui/card';
 
-export const SongCard = ({ song }: SongProps) => {
+// Vibe Card
+const VibeCard = ({ song }: SongProps) => {
   const [isPlaying, setIsPlaying] = useState(false);
   // const [isFavorite, setIsFavorite] = useState(false);
 
@@ -46,9 +47,9 @@ export const SongCard = ({ song }: SongProps) => {
               onClick={() => setIsPlaying(!isPlaying)}
             >
               {isPlaying ? (
-                <Pause className="size-7 sm:size-5 md:size-7 lg:size-5 fill-foreground stroke-0" />
+                <Pause className="stroke-0 size-5 md:size-6 lg:size-5 fill-foreground" />
               ) : (
-                <Play className="size-7 sm:size-5 md:size-7 lg:size-5 fill-foreground stroke-0" />
+                <Play className="stroke-0 size-5 md:size-6 lg:size-5 fill-foreground" />
               )}
             </MotionButton>
           </div>
@@ -77,3 +78,60 @@ export const SongCard = ({ song }: SongProps) => {
     </Card>
   );
 };
+
+// Album Card
+const AlbumCard = ({ song }: SongProps) => {
+  const [isPlaying, setIsPlaying] = useState(false);
+  // const [isFavorite, setIsFavorite] = useState(false);
+
+  return (
+    <Card className="p-0 bg-transparent border-none rounded-none">
+      <CardContent className="p-0">
+        <AspectRatio
+          ratio={1}
+          className="relative overflow-hidden rounded-xs"
+          // onClick={() => {
+          //   // playTrack(song, navigate);
+          // }}
+        >
+          <Image
+            src={song.songCover.coverUrl || song.defaultCover}
+            alt={song.title}
+            className="object-cover size-full"
+            loading="lazy"
+            width={1200}
+            height={1200}
+          />
+          <div className="absolute flex justify-end bottom-2 left-2">
+            <MotionButton
+              className="bg-gradient-to-t from-black/60 to-transparent shrink-0"
+              onClick={() => setIsPlaying(!isPlaying)}
+            >
+              {isPlaying ? (
+                <Pause className="stroke-0 size-7 sm:size-5 md:size-7 lg:size-5 fill-foreground" />
+              ) : (
+                <Play className="stroke-0 size-7 sm:size-5 md:size-7 lg:size-5 fill-foreground" />
+              )}
+            </MotionButton>
+          </div>
+        </AspectRatio>
+      </CardContent>
+
+      <CardFooter className="flex flex-col items-start gap-2 p-0">
+        <CardTitle className="w-full capitalize truncate line-clamp-1 sm:text-base lg:text-sm">
+          {song.title}
+        </CardTitle>
+        <CardDescription className="flex flex-col justify-between w-full gap-2 sm:gap-0 sm:flex-row sm:items-center">
+          <span
+            className="font-medium capitalize min-w-auto sm:max-w-[70%] 
+          truncate transition-colors duration-100 ease-in cursor-pointer select-none hover:text-foreground sm:text-sm lg:text-xs line-clamp-1"
+          >
+            {song.artist}
+          </span>
+        </CardDescription>
+      </CardFooter>
+    </Card>
+  );
+};
+
+export { VibeCard, AlbumCard };
