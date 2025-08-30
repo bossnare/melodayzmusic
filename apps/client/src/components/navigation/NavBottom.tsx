dafd98ea0a279d2f29337a9156f2f58e48fc23ee
Christo Razafimanga <144788145+bossnare@users.noreply.github.com>
2025-08-31 00:10:06 +0300

    Update NavBottom.tsx: add gap on NavBottom align

--- apps/client/src/components/navigation/NavBottom.tsx
+++ apps/client/src/components/navigation/NavBottom.tsx
@@ -6,7 +6,7 @@ import { Tabs } from './Tab';
 
 export const NavBottom = () => {
   return (
-    <ul className="flex items-center justify-center pt-1 pb-10 transition-all duration-300 ease-in-out sm:pb-6 sm:gap-0 md:py-2 lg:hidden">
+    <ul className="flex items-center justify-center gap-1 pt-1 pb-10 transition-all duration-300 ease-in-out sm:pb-6 sm:gap-0 md:py-2 lg:hidden">
       {navLabels.map((tab) => (
         <li
           className="flex items-center justify-center w-[calc(100%/5)] md:w-[calc(100%/4-6px)] shrink-0 text-xs"
@@ -26,3 +26,4 @@ export const NavBottom = () => {
     </ul>
   );
 };
+
