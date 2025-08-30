ee2f18a8045e7e976095cbe97178ea672ace6568
Christo Razafimanga <144788145+bossnare@users.noreply.github.com>
2025-08-31 00:23:35 +0300

    Update NavBottom.tsx: update tab width to 100/4.9

--- apps/client/src/components/navigation/NavBottom.tsx
+++ apps/client/src/components/navigation/NavBottom.tsx
@@ -9,7 +9,7 @@ export const NavBottom = () => {
     <ul className="flex items-center justify-center gap-1 pt-1 pb-10 transition-all duration-300 ease-in-out sm:pb-6 sm:gap-0 md:py-2 lg:hidden">
       {navLabels.map((tab) => (
         <li
-          className="flex items-center justify-center w-[calc(100%/4.8)] md:w-[calc(100%/4-6px)] shrink-0 text-xs"
+          className="flex items-center justify-center w-[calc(100%/4.9)] md:w-[calc(100%/4-6px)] shrink-0 text-xs"
           key={tab.id}
         >
           {tab.label === 'create' ? (
@@ -26,5 +26,6 @@ export const NavBottom = () => {
     </ul>
   );
 };
+
 
 
