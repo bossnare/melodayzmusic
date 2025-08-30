e9e3d3af91b4c46884f81306828a1c5dc6dd3a39
bossnare <razafimangagervaischristo@gmail.com>
2025-08-30 09:22:24 +0300

    client: test lenis

--- apps/client/src/app/dashboard/SmoothScrollLayout.tsx
+++ apps/client/src/app/dashboard/SmoothScrollLayout.tsx
@@ -1,8 +1,32 @@
 'use client';
 
 import type { BaseProps } from '@/types/base.interface';
+import Lenis from 'lenis';
+import { useEffect } from 'react';
 
 const SmoothScrollLayout = ({ children }: BaseProps) => {
+  useEffect(() => {
+    const wrapper = document.querySelector('#main-content') as HTMLDivElement;
+    if (!wrapper) return;
+    const lenis = new Lenis({
+      wrapper,
+      content: wrapper,
+      autoRaf: true,
+      duration: 1.2,
+      orientation: 'vertical',
+      smoothTouch: true,
+      gestureOrientation: 'vertical',
+    });
+
+    function raf(time: number) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+    requestAnimationFrame(raf);
+
+    return () => lenis.destroy();
+  }, []);
+
   return (
     <div
       id="main-content"
