2b86375872bf9ab2625180bf24f0b23c615a63c2
bossnare <razafimangagervaischristo@gmail.com>
2025-08-30 20:50:27 +0300

    client:ui: optimize icon logo on login page

--- apps/client/src/app/auth/login/page.tsx
+++ apps/client/src/app/auth/login/page.tsx
@@ -106,7 +106,7 @@ export default function LoginPage() {
                 </FormItem>
               )}
             ></FormField>
-            <Button className="cta" disabled={true} type="submit">
+            <Button className="rounded-full cta" disabled={true} type="submit">
               Se connecter
             </Button>
           </form>
