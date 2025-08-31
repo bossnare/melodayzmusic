ca9ee7d71a8353c30e91a9a36b8225c980248c9f
Christo Razafimanga <144788145+bossnare@users.noreply.github.com>
2025-08-31 23:46:17 +0300

    Update page.tsx: drop loading text and update CTA text  to verb action

--- apps/client/src/app/page.tsx
+++ apps/client/src/app/page.tsx
@@ -23,15 +23,16 @@ export default function Page() {
         available or may change frequently. Thank you for your patience and
         understanding as we work to improve your experience!
       </p>
-      <Button variant="secondary" onClick={handleClickTab} size="lg">
+      <Button variant="secondary" onClick={handleClickTab} size="md">
         {isPending ? (
           <>
-            <LoaderCircle className="animate-spin" /> <span>loading...</span>
+            <LoaderCircle className="animate-spin" />
           </>
         ) : (
-          'Login'
+          'Se commencer'
         )}
       </Button>
     </div>
   );
 }
+
