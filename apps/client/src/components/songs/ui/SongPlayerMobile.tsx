f80e24505ae541aba1987507cf6b7cd944436bb5
Christo Razafimanga <144788145+bossnare@users.noreply.github.com>
2025-09-03 01:09:13 +0300

    Update SongPlayerMobile.tsx: spacing

--- apps/client/src/components/songs/ui/SongPlayerMobile.tsx
+++ apps/client/src/components/songs/ui/SongPlayerMobile.tsx
@@ -1,22 +1,22 @@
 import { MotionButton } from '@/components/motions/motionButton';
-import { Pause, Music, Heart, SkipBack, SkipForward } from 'lucide-react';
+import { Play, Music, Heart, SkipBack, SkipForward } from 'lucide-react';
 
 const SongPlayerMobile = () => {
   return (
-    <div className="fixed inset-x-0 flex justify-between h-16 gap-2 px-2 py-2 lg:hidden bottom-16 dark:bg-nav">
-      <div className="p-2 border rounded-md shadow-sm border-secondary-foreground">
+    <div className="fixed inset-x-0 flex items-center h-16 gap-2 px-2 py-2 lg:hidden bottom-16 dark:bg-nav">
+      <div className="p-1 border rounded-md shadow-sm border-secondary-foreground">
         <Music className="size-10 text-muted-foreground" />
       </div>
       {/* info */}
       <div className="flex flex-col items-start justify-center gap-1 text-muted-foreground">
         <p>Aucune lecture</p>
       </div>
-      <div className="flex items-center gap-1 *:text-muted-foreground">
+      <div className="flex items-center gap-1 *:text-muted-foreground ml-auto">
         <MotionButton>
           <SkipBack className="size-auto" />
         </MotionButton>
         <MotionButton>
-          <Pause className="fill-current size-8" />
+          <Play className="fill-current size-8" />
         </MotionButton>
         <MotionButton>
           <SkipForward className="size-auto" />
