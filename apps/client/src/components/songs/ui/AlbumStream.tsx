cc09b70a87a35e5880c00d4874600f928de06f95
Christo Razafimanga <144788145+bossnare@users.noreply.github.com>
2025-09-02 13:31:15 +0300

    Update AlbumStream.tsx

--- apps/client/src/components/songs/ui/AlbumStream.tsx
+++ apps/client/src/components/songs/ui/AlbumStream.tsx
@@ -6,7 +6,7 @@ import { useEmblaProgress } from '@/hooks/useEmblaProgress';
 const AlbumStream = ({ children }: AlbumProps) => {
   const { emblaRef, showFadeStart, showFadeEnd } = useEmblaProgress(
     true,
-    1,
+    2,
     0.6,
     'center'
   );
@@ -20,7 +20,7 @@ const AlbumStream = ({ children }: AlbumProps) => {
           ref={emblaRef}
           className="overflow-hidden scroll-smooth scrollbar-none"
         >
-          <div className="grid grid-flow-col auto-cols-[calc(100vw)] sm:auto-cols-[calc(100vw/4.6)] lg:auto-cols-[calc(100vw/8)] gap-4 lg:gap-5">
+          <div className="grid grid-flow-col auto-cols-[calc(100vw/1.5)] sm:auto-cols-[calc(100vw/4.6)] lg:auto-cols-[calc(100vw/8)] gap-4 lg:gap-5">
             {children}
           </div>
         </div>
