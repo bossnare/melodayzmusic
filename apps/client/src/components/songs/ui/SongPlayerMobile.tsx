dbeb721167d8c9945657faaf8233ad11b70b8190
Christo Razafimanga <144788145+bossnare@users.noreply.github.com>
2025-09-03 01:00:07 +0300

    Update SongPlayerMobile.tsx: change color to muted foreground

--- apps/client/src/components/songs/ui/SongPlayerMobile.tsx
+++ apps/client/src/components/songs/ui/SongPlayerMobile.tsx
@@ -3,20 +3,20 @@ import { Pause, Music, Heart, SkipBack, SkipForward } from 'lucide-react';
 
 const SongPlayerMobile = () => {
   return (
-    <div className="fixed inset-x-0 flex justify-between h-16 gap-2 px-2 py-1 lg:hidden bottom-16 dark:bg-nav">
+    <div className="fixed inset-x-0 flex justify-between h-16 gap-2 px-2 py-2 lg:hidden bottom-16 dark:bg-nav">
       <div className="p-2 border rounded-md shadow-sm border-secondary-foreground">
-        <Music className="size-10 text-muted-foreground/70" />
+        <Music className="size-10 text-muted-foreground" />
       </div>
       {/* info */}
-      <div className="flex flex-col items-start justify-center gap-1 text-muted">
-        <p>No current song played</p>
+      <div className="flex flex-col items-start justify-center gap-1 text-muted-foreground">
+        <p>Aucune lecture</p>
         <span>
           <MotionButton className="p-0">
             <Heart className="size-auto" />
           </MotionButton>
         </span>
       </div>
-      <div className="flex items-center gap-1 *:text-muted">
+      <div className="flex items-center gap-1 *:text-muted-foreground">
         <MotionButton>
           <SkipBack className="size-auto" />
         </MotionButton>
