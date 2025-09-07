7e5d10d983a6dad47397baea90afbf4495ad7075
Christo Razafimanga <144788145+bossnare@users.noreply.github.com>
2025-09-07 22:59:44 +0300

    Update AuthCard.tsx: change icon Tickets

--- apps/client/src/components/auth/AuthCard.tsx
+++ apps/client/src/components/auth/AuthCard.tsx
@@ -21,7 +21,7 @@ import { StepCardWrapper } from './AuthWrapper';
 import { Divide } from './Divide';
 import { PasswordInput } from './PasswordInput';
 import { Provider } from './Provider';
-import { Tickets } from 'lucide-react';
+import { Mail } from 'lucide-react';
 
 const loginSchema = z.object({
   email: z.string().email(),
@@ -282,7 +282,7 @@ const StepThreeCard = () => {
     <StepCardWrapper key="step3">
       <Card className="p-4 dark:bg-transparent lg:p-5">
         <CardTitle className="text-base text-center">
-          Ton email, ton VIP <Tickets />
+          Ton email, ton VIP <Mail />
         </CardTitle>
         {/* Form Content */}
         <CardContent className="flex flex-col p-1 md:flex-row">
