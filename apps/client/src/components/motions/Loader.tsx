a9d089a1855613c2500269367e1c94358b8e071d
bossnare <razafimangagervaischristo@gmail.com>
2025-09-12 22:06:16 +0300

    client: update loader

--- apps/client/src/components/motions/Loader.tsx
+++ apps/client/src/components/motions/Loader.tsx
@@ -1,7 +1,7 @@
 export const Loader = ({ className }: { className: string }) => {
   return (
     <div
-      className={`animate-spin border-4 rounded-full !border-t-transparent ${className}`}
+      className={`animate-spin border-4 rounded-full !border-t-transparent !border-l-foreground/50 ${className}`}
     ></div>
   );
 };
