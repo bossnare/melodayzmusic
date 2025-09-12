8d807ce76d7152842eeef2d36006178dd7e595dd
Christo Razafimanga <144788145+bossnare@users.noreply.github.com>
2025-09-13 00:00:41 +0300

    Update Loader.tsx

--- apps/client/src/components/motions/Loader.tsx
+++ apps/client/src/components/motions/Loader.tsx
@@ -1,7 +1,7 @@
 export const Loader = ({ className }: { className: string }) => {
   return (
     <div
-      className={`animate-spin border-4 rounded-full !border-t-transparent !border-l-foreground/20 ${className}`}
+      className={`animate-spin border-4 rounded-full !border-t-transparent !border-l-muted ${className}`}
     ></div>
   );
 };
