f8b7edbd4b5084be3e78c54340fc262f36d03bd7
Christo Razafimanga <144788145+bossnare@users.noreply.github.com>
2025-09-13 23:04:37 +0300

    Update SongPlayerMobile.tsx: add shadow

--- apps/client/src/components/songs/ui/SongPlayerMobile.tsx
+++ apps/client/src/components/songs/ui/SongPlayerMobile.tsx
@@ -11,7 +11,7 @@ const SongPlayerMobile = () => {
         exit={{ y: -20, opacity: 0 }}
         transition={{ type: 'spring', stiffness: 500, damping: 30 }}
         key={'PlayerMobile'}
-        className="fixed inset-x-0 flex items-center h-20 gap-2 px-2 py-2 lg:hidden bottom-16 bg-nav"
+        className="fixed inset-x-0 flex items-center h-20 gap-2 px-2 py-2 lg:hidden bottom-16 bg-nav shadow-md"
       >
         <div className="p-1 border rounded-md shadow-sm border-foreground dark:border-secondary-foreground">
           <Music className="size-10 text-muted-foreground" />
