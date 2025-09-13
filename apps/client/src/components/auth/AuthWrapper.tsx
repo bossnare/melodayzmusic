715f3c0c86a9fbacf4bfed1ea51e46298cce0bd2
bossnare <razafimangagervaischristo@gmail.com>
2025-09-13 09:11:55 +0300

    client: formating...

--- apps/client/src/components/auth/AuthWrapper.tsx
+++ apps/client/src/components/auth/AuthWrapper.tsx
@@ -21,7 +21,10 @@ const StepCardWrapper = ({ children, key }: BaseProps & { key: string }) => {
 
 const AuthPageWrapper = ({ children }: BaseProps) => {
   return (
-    <section className="relative flex flex-col items-center justify-between gap-2 px-4 bg-gradient-to-br pb-[2rem] dark:from-primary/8 via-transparent dark:to-ring/8 from-primary/25 to-ring/20 md:h-screen h-dvh md:pb-4">
+    <section
+      className="relative flex flex-col items-center justify-between gap-2 px-4 bg-gradient-to-br pb-[2rem] dark:from-primary/8 
+    via-transparent dark:to-ring/8 from-primary/25 to-ring/20 md:h-screen h-dvh md:pb-4"
+    >
       {children}
       {/* tag */}
       <Tagline />
