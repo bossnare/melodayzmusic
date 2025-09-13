75a11df86600e29d1eb4d4b615f464c7d4d2d4cf
bossnare <razafimangagervaischristo@gmail.com>
2025-09-13 09:55:15 +0300

    client:ui: update loader and authHeaderSwitch padding

--- apps/client/src/components/auth/AuthHeaderSwitch.tsx
+++ apps/client/src/components/auth/AuthHeaderSwitch.tsx
@@ -22,7 +22,7 @@ const AuthHeaderSwitch = ({
   const router = useRouter();
 
   return (
-    <nav className="flex items-center justify-between w-full gap-12 py-3">
+    <nav className="flex items-center justify-between w-full gap-12 py-2">
       {type === 'login' && (
         <>
           <p className="text-sm">Vos vibes n&apos;attendent que vous.</p>
