6f331c09c87962898c00c467f09054c9c311cbf8
bossnare <razafimangagervaischristo@gmail.com>
2025-09-15 14:06:19 +0300

    client:ui:

--- apps/client/src/app/dashboard/layout.tsx
+++ apps/client/src/app/dashboard/layout.tsx
@@ -53,7 +53,7 @@ export default function DashboardLayout({
           {/* Main Layout */}
           <SmoothScrollLayout>
             {/* Main content */}
-            <main className="px-4 sm:px-6 pb-[40rem] lg:pb-80">{children}</main>
+            <main className="px-4 sm:px-6 pb-[30rem] lg:pb-80">{children}</main>
           </SmoothScrollLayout>
         </div>
 
