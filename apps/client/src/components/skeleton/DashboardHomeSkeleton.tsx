4910261e1e266faf0e53f194ec7d38ab5063df69
Christo Razafimanga <144788145+bossnare@users.noreply.github.com>
2025-09-15 22:34:37 +0300

    Update DashboardHomeSkeleton.tsx: width on title section

--- apps/client/src/components/skeleton/DashboardHomeSkeleton.tsx
+++ apps/client/src/components/skeleton/DashboardHomeSkeleton.tsx
@@ -19,7 +19,7 @@ export const DashboardHomeSkeleton = () => {
 
       {/* Grid Skeleton */}
       <div>
-        <Skeleton className="w-40 h-6 mb-2 lg:mb-3 rounded-xl bg-foreground/25" />
+        <Skeleton className="w-30 h-6 mb-2 lg:mb-3 rounded-xl bg-foreground/25" />
         <div
           className="grid grid-flow-col auto-cols-[calc(100vw/2.5)] sm:auto-cols-[calc(100vw/4.5)] 
         lg:auto-cols-[calc(100vw/9)] xl:auto-cols-[calc(100vw/8.5)] gap-4 lg:gap-5"
@@ -44,3 +44,4 @@ export const DashboardHomeSkeleton = () => {
     </div>
   );
 };
+
