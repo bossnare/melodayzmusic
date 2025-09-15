5ec5fce016746c279e241638f224ac53e7c651b8
bossnare <razafimangagervaischristo@gmail.com>
2025-09-15 12:55:26 +0300

    client:ux: damping to 0.0001 on smooth scroll plugin

--- apps/client/src/app/dashboard/SmoothScrollLayout.tsx
+++ apps/client/src/app/dashboard/SmoothScrollLayout.tsx
@@ -11,7 +11,7 @@ const SmoothScrollLayout = ({ children }: BaseProps) => {
     if (!scrollRef.current) return;
 
     Scrollbar.init(scrollRef.current, {
-      damping: 1, // inertia feel
+      damping: 0.0001, // inertia feel
       renderByPixels: true,
       alwaysShowTracks: false,
     });
