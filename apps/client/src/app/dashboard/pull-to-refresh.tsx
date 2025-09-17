0c2511dee2fbfb5be9797f028eb82cb80b4ec0bf
Christo Razafimanga <144788145+bossnare@users.noreply.github.com>
2025-09-17 20:38:08 +0300

    Update pull-to-refresh.tsx

--- apps/client/src/app/dashboard/pull-to-refresh.tsx
+++ apps/client/src/app/dashboard/pull-to-refresh.tsx
@@ -2,7 +2,7 @@ import PullToRefresh from 'react-simple-pull-to-refresh'
 
 export default function RefreshWrapper({children, onRefresh}: {children: React.ReactNode, onRefresh: () => Promise<void>}) {
   return (
-    <PullToRefresh onRefresh={onRefresh}>
+    <PullToRefresh pullDownThreshold={80} resistance={2.5} onRefresh={onRefresh}>
       {children}
     </PullToRefresh>
   )
