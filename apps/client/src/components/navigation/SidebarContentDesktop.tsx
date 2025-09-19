c4babeacd7c27cfead825b9cb69b30b26794e78c
bossnare <razafimangagervaischristo@gmail.com>
2025-09-19 09:33:13 +0300

    client:ui: add fallback profilPicture

--- apps/client/src/components/navigation/SidebarContentDesktop.tsx
+++ apps/client/src/components/navigation/SidebarContentDesktop.tsx
@@ -10,7 +10,11 @@ const SidebarContentDesktop = () => {
       <figure className="flex items-center w-full gap-2 mt-4 mb-3">
         <figcaption className="flex gap-3 cursor-pointer grow active:bg-muted/80 lg:hover:bg-muted/50">
           <Avatar className="size-10 ring-2 ring-primary">
-            <AvatarImage />
+            <AvatarImage
+                        className="object-cover"
+                        alt="omahlay"
+                        src="/img/profil/omah_lay.jpg"
+                      />
             <AvatarFallback>U</AvatarFallback>
           </Avatar>
           <div className="flex flex-col">
