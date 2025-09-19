8a703d83733a52407bd7beb287b5eadfcc0bc465
bossnare <razafimangagervaischristo@gmail.com>
2025-09-19 19:32:07 +0300

    server: drop duplicate import on main

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,11 +1,10 @@
 import { NestFactory, Reflector } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { SwaggerModule } from '@nestjs/swagger';
-import * as cookieParser from 'cookie-parser';
 import { Response } from 'express';
 import helmet from 'helmet';
 import morgan from 'morgan';
-import cookieParser from 'cookie-parser'
+import cookieParser from 'cookie-parser';
 // import { dirname, join } from 'path';
 // import { fileURLToPath } from 'url';
 import { Logger, ValidationPipe } from '@nestjs/common';
