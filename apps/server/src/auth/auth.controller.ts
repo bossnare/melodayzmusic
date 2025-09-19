f7776bb07a873caaa8b95b732c4440a03ee079f5
bossnare <razafimangagervaischristo@gmail.com>
2025-09-19 21:46:44 +0300

    server: secure for production only on cookies set

--- apps/server/src/auth/auth.controller.ts
+++ apps/server/src/auth/auth.controller.ts
@@ -31,7 +31,7 @@ export class AuthController {
 
     res.cookie('access_token', access_token, {
       httpOnly: true,
-      secure: true,
+      secure: process.env.NODE_ENV === 'production',
       sameSite: 'none',
       maxAge: 1000 * 60 * 60 * 24,
     });
