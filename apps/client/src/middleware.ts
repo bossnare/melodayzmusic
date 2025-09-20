5875d0cff8d4899c6b95d7b88ba5caf00e9455f3
Christo Razafimanga <144788145+bossnare@users.noreply.github.com>
2025-09-20 14:25:01 +0300

    Update middleware.ts: skip instead dashboard on matcher

--- apps/client/src/middleware.ts
+++ apps/client/src/middleware.ts
@@ -22,5 +22,5 @@ export default function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/dashboard/:path*', '/auth/:path*', '/'],
+  matcher: ['/dashboar/:path*', '/auth/:path*', '/'],
 };
