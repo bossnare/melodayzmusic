9ed98a56d811ded154fdc32fb174095b4d1345da
bossnare <razafimangagervaischristo@gmail.com>
2025-09-21 09:55:22 +0300

    server:auth: add path on set cookie

--- apps/client/src/middleware.ts
+++ apps/client/src/middleware.ts
@@ -22,5 +22,5 @@ export default function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/dashboar/:path*', '/auth/:path*', '/'],
+  matcher: ['/dashboard/:path*', '/auth/:path*', '/'],
 };
