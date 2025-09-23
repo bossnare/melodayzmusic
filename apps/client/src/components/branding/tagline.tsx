6716904d59346f222058c257bb54d516ee0ad8fd
Christo Razafimanga <144788145+bossnare@users.noreply.github.com>
2025-09-23 22:21:03 +0300

    Update tagline.tsx: update py

--- apps/client/src/components/branding/tagline.tsx
+++ apps/client/src/components/branding/tagline.tsx
@@ -3,7 +3,7 @@ import { ModeToggle } from '../themes/mode-toggle';
 export const Tagline = () => {
   return (
     <div className="text-xs text-foreground/90 font-montserrat flex flex-col items-center">
-      <div className="py-2 lg:hidden">
+      <div className="py-3 lg:hidden">
         <ModeToggle />
       </div>
       Feel the Beat, Anywhere You Go.
