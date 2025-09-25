89e8ccb26b4dd0bd560c47a781346c5badee8437
bossnare <razafimangagervaischristo@gmail.com>
2025-09-25 12:02:55 +0300

    client:ui: add py on cta button and providers button

--- apps/client/src/components/auth/AuthCtaButton.tsx
+++ apps/client/src/components/auth/AuthCtaButton.tsx
@@ -24,7 +24,7 @@ export const AuthCtaButton = ({
       size="lg"
       className={cn(
         isPending && 'pointer-events-none',
-        'relative overflow-hidden bg-premium',
+        'relative overflow-hidden bg-premium py-4 lg:py-5',
         className
       )}
       type="submit"
--- apps/client/src/components/auth/Provider.tsx
+++ apps/client/src/components/auth/Provider.tsx
@@ -9,7 +9,7 @@ export function Provider() {
         <li key={provider.id}>
           <Button
             variant="secondary"
-            className="w-full rounded-full md:w-auto"
+            className="w-full rounded-full md:w-auto py-4 lg:py-5"
             size="lg"
           >
             {provider.label === 'Google' ? (
