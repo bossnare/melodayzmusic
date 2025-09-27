52a116f6b2ef876b0933b452c96c5cf2f948973b
bossnare <razafimangagervaischristo@gmail.com>
2025-09-27 16:48:14 +0300

    client:aut:ux: add alert icon for login error dialog

--- apps/client/src/components/auth/DialogCloseButton.tsx
+++ apps/client/src/components/auth/DialogCloseButton.tsx
@@ -8,6 +8,7 @@ import {
   DialogHeader,
   DialogTitle,
 } from '@/components/ui/dialog';
+import { AlertTriangle } from 'lucide-react';
 
 export function DialogCloseButton({
   title,
@@ -15,17 +16,20 @@ export function DialogCloseButton({
   close,
   open,
   onOpenChange,
+  error = true,
 }: {
   title: string;
   description: string;
   close: string;
   open: boolean;
   onOpenChange: () => void;
+  error?: boolean;
 }) {
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
       <DialogContent className="sm:max-w-md">
         <DialogHeader>
+          {error && <AlertTriangle className="text-destructive mx-auto mb-2" />}
           <DialogTitle>{title}</DialogTitle>
           <DialogDescription>{description}</DialogDescription>
         </DialogHeader>
