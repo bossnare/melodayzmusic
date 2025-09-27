98e065d3fd10d6b5abb263080c31de583038a827
Christo Razafimanga <144788145+bossnare@users.noreply.github.com>
2025-09-27 22:31:42 +0300

    Update page.tsx: comment revalidate variable

--- apps/client/src/app/auth/register/page.tsx
+++ apps/client/src/app/auth/register/page.tsx
@@ -4,7 +4,7 @@ import { RegisterCard } from '@/components/auth/AuthCard';
 import { AuthHeaderSwitch } from '@/components/auth/AuthHeaderSwitch';
 import { AuthPageWrapper } from '@/components/auth/AuthWrapper';
 
-export const revalidate = 0;
+// export const revalidate = 0;
 
 export default function RegisterPage() {
   return (
