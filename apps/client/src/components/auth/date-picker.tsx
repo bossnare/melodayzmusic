81e9d9fd873deccf778812dd9509e9d497dd3721
Christo Razafimanga <144788145+bossnare@users.noreply.github.com>
2025-10-05 02:22:39 +0300

    Update date-picker.tsx: convert date to ISOString

--- apps/client/src/components/auth/date-picker.tsx
+++ apps/client/src/components/auth/date-picker.tsx
@@ -50,7 +50,7 @@ export function DatePicker({
             selected={value}
             captionLayout="dropdown"
             onSelect={(date) => {
-              onChangeAction?.(date);
+              onChangeAction?.(new Date(date.toISOString()));
               setDate(date);
               setOpen(false);
             }}
