c8b5f6121c667c80181760e76685ed2a88f0b70a
bossnare <razafimangagervaischristo@gmail.com>
2025-10-04 08:41:31 +0300

    client:ux:fix: check icon transition on username input

--- apps/client/src/components/auth/CustomInput.tsx
+++ apps/client/src/components/auth/CustomInput.tsx
@@ -78,13 +78,11 @@ const UsernameInput = ({
             opacity: 0,
             scale: 0.5,
             rotate: -45,
-            y: -20,
           }}
           animate={{
             opacity: 1,
-            scale: 1,
+            scale: [0.8, 1.2, 1], // overshoot
             rotate: 0,
-            y: 0,
           }}
           transition={{
             duration: 0.6,
