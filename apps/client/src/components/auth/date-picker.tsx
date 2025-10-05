31ee74f534c4eb3d3d697ad3026cfda091a1ce93
Christo Razafimanga <144788145+bossnare@users.noreply.github.com>
2025-10-05 19:22:02 +0300

    Update date-picker.tsx: test with undefined value on selected date of is null

--- apps/client/src/components/auth/date-picker.tsx
+++ apps/client/src/components/auth/date-picker.tsx
@@ -47,7 +47,7 @@ export function DatePicker({
         <PopoverContent className="w-auto overflow-hidden p-0" align="start">
           <Calendar
             mode="single"
-            selected={new Date(value || date || '')}
+            selected={new Date(value || date || undefined)}
             captionLayout="dropdown"
             onSelect={(date) => {
               onChangeAction?.(date && date.toISOString());
