730770fb41c14af0dc1367c1d74c99cd7580aee4
bossnare <razafimangagervaischristo@gmail.com>
2025-10-06 13:16:33 +0300

    client:ux:fix: bad error

--- apps/client/src/components/auth/CustomInput.tsx
+++ apps/client/src/components/auth/CustomInput.tsx
@@ -59,7 +59,15 @@ const UsernameInput = ({
   ...props
 }: UsernameInputProps) => {
   function renderingStatus() {
-    if (isPending || autocheckLoading) {
+    if (isPending) {
+      return (
+        <span className="absolute -translate-y-1/2 right-2 lg:right-3 top-1/2 p-[2px] rounded-full">
+          <Spinner className="size-[18px] text-foreground/50" />
+        </span>
+      );
+    }
+
+    if (autocheckLoading) {
       return (
         <span className="absolute -translate-y-1/2 right-2 lg:right-3 top-1/2 p-[2px] rounded-full">
           <Spinner className="size-[18px] text-foreground/50" />
