f222a8b01cbe6befc70e79cc8a800ab6d0ad0c94
Christo Razafimanga <144788145+bossnare@users.noreply.github.com>
2025-10-16 21:40:03 +0300

    Change loading indicator style in OtpOverlay

--- apps/client/src/components/auth/OtpOverlay.tsx
+++ apps/client/src/components/auth/OtpOverlay.tsx
@@ -95,7 +95,7 @@ function OtpOverlay() {
             Pour confirmer que cette adresse t&apos;appartient, nous
             t&apos;avons envoyé un code à 6 chiffres à ton adresse e-mail{' '}
             <span className="text-foreground/90 md:text-sm tracking-wide">
-              {email === '' ? <div className="animate-spin h-6 w-50 bg-muted rounded"></div> : email}
+              {email === '' ? <div className="animate-pulse h-5 w-50 bg-muted rounded-sm"></div> : email}
             </span>{' '}
           </p>
           <InputOTPPattern />
