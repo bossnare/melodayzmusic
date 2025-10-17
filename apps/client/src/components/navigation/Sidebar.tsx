b39e8bf68a6b2131ab6c4bb1de8bfce614011cdb
Christo Razafimanga <144788145+bossnare@users.noreply.github.com>
2025-10-17 22:57:19 +0300

    Refactor Sidebar component for better UX
    Updated the Sidebar component to improve user experience and loading states.

--- apps/client/src/components/navigation/Sidebar.tsx
+++ apps/client/src/components/navigation/Sidebar.tsx
@@ -91,7 +91,7 @@ export const Sidebar = ({
           </SheetTitle>
           {/* content */}
           <SheetDescription className="px-4">
-            <figure className="flex items-center w-full gap-4 mt-2 mb-3">
+            <figure className="flex w-full gap-4 mt-2 mb-3">
               <figcaption className="flex gap-3 cursor-pointer grow active:bg-muted lg:hover:bg-muted/50">
                 <Avatar className="size-14 ring-2 border border-current ring-muted">
                   <AvatarImage
@@ -103,14 +103,17 @@ export const Sidebar = ({
                 </Avatar>
                 <div className="flex flex-col w-full">
                   {fetchingMe ? (
-                    <div className="w-8/9 h-10 rounded bg-foreground/30 animate-pulse"></div>
+                   <>
+                    <div className="w-8/9 h-5 rounded-sm bg-foreground/30 animate-pulse"></div>
+                     <div className="w-4/5 h-2 mt-1 rounded-sm bg-foreground/30 animate-pulse"></div>
+                   </>
                   ) : (
                     <>
                       <span className="text-base capitalize font-semibold font-montserrat text-foreground/80">
                         {user?.pseudo}
                       </span>
                       <span className="text-[14px] text-muted-foreground">
-                        {user?.role === 'USER' ? 'Fan' : 'Utilisateur(e)'}
+                        @{user?.username} • {user?.role === 'USER' ? 'Fan' : 'Utilisateur(e)'}
                       </span>
                     </>
                   )}
@@ -170,3 +173,4 @@ export const Sidebar = ({
     </>
   );
 };
+
