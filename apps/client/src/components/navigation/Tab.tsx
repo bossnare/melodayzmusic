17d1a1188c58f2a380e76892ccd7f58cc358b2c4
bossnare <razafimangagervaischristo@gmail.com>
2025-10-17 18:06:57 +0300

    client:reduce loader on mobile bottom tab

--- apps/client/src/components/navigation/Tab.tsx
+++ apps/client/src/components/navigation/Tab.tsx
@@ -24,7 +24,7 @@ export const Tabs = ({ href, Icon, label }: TabProps) => {
       >
         <MotionTab className="*:!size-7 lg:*:!size-6">
           {isPending && !isActive ? (
-            <Loader className="border-foreground/90 !size-4" />
+            <Loader className="border-foreground/90 !size-3 lg:!size-4" />
           ) : (
             <Icon weight={isActive ? 'fill' : 'bold'} />
           )}
