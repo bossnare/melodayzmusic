1673c9046d0678d98369774b5386d9973846cde0
bossnare <razafimangagervaischristo@gmail.com>
2025-10-18 10:29:25 +0300

    client:ux:fix: profile page and Loader on tab

--- apps/client/src/app/dashboard/profile/page.tsx
+++ apps/client/src/app/dashboard/profile/page.tsx
@@ -29,7 +29,7 @@ export default function ProfilePage() {
           <AvatarFallback>US</AvatarFallback>
         </Avatar>
         {/* name */}
-        <div className="space-y-1">
+        <div className="space-y-1 grow">
           {isFetchingMe ? (
             <>
               <Skeleton className="w-8/9 h-8 bg-foreground/30" />
