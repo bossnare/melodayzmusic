d41307c37688efca45b27a3f6a60a5672897ea88
bossnare <razafimangagervaischristo@gmail.com>
2025-10-19 21:44:55 +0300

    client:ux:fix: wrapper for auth page

--- apps/client/src/app/auth/register/page.tsx
+++ apps/client/src/app/auth/register/page.tsx
@@ -11,7 +11,7 @@ export default function RegisterPage() {
     <AuthPageWrapper>
       <AuthHeaderSwitch href={'/auth/login'} type="back" />
 
-      <div className="w-full lg:w-[60%]">
+      <div className="w-full px-2 lg:w-[60%]">
         <RegisterCard />
       </div>
     </AuthPageWrapper>
