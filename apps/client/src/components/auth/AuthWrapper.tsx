fd93d3f9c41473d527917409383af5d78f4c4165
bossnare <razafimangagervaischristo@gmail.com>
2025-10-19 21:57:55 +0300

    client:ux: adjust div wrapper on md and more

--- apps/client/src/components/auth/AuthWrapper.tsx
+++ apps/client/src/components/auth/AuthWrapper.tsx
@@ -36,7 +36,9 @@ const AuthPageWrapper = ({
       className="relative flex flex-col items-center justify-between gap-4 sm:gap-6 lg:gap-2 md:px-10 pb-[1.5rem]
       sm:h-auto lg:h-dvh h-dvh md:pb-4"
     >
-      <div className="w-full">{children}</div>
+      <div className="w-full md:flex md:flex-col md:justify-center md:items-center">
+        {children}
+      </div>
       {/* Overlay */}
       <Overlay isPending={isPending} textLoading={textLoading} />
       {/* tag */}
