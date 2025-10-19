2f1660b5f8fd0293cf35c21f98eccefc2f55eb6e
Christo Razafimanga <144788145+bossnare@users.noreply.github.com>
2025-10-19 19:40:50 +0300

    Update song status text in SongPlayerMobile

--- apps/client/src/components/songs/ui/SongPlayerMobile.tsx
+++ apps/client/src/components/songs/ui/SongPlayerMobile.tsx
@@ -27,7 +27,7 @@ const SongPlayerMobile = () => {
           {/* info */}
           <div className="flex flex-col items-start justify-center gap-[2px]">
             <p className="font-semibold text-muted-foreground/80">
-              Pas en lecture
+              En attente de vibes
             </p>
             <p className="text-xs text-muted-foreground">Artiste inconnu</p>
           </div>
