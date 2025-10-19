f098b980fa903d93c668af8aca60335025d81872
Christo Razafimanga <144788145+bossnare@users.noreply.github.com>
2025-10-19 23:01:41 +0300

    Adjust initial scale in AuthCard animation

--- apps/client/src/components/auth/AuthCard.tsx
+++ apps/client/src/components/auth/AuthCard.tsx
@@ -59,7 +59,7 @@ function LoginCard({
         <motion.div
           key={pathname + 'login'}
           className="w-full lg:w-3/4"
-          initial={{ scale: 1.3, opacity: 0 }}
+          initial={{ scale: 1.2, opacity: 0 }}
           animate={{ scale: 1, opacity: 1 }}
           exit={{ y: 0.8, opacity: 0 }}
           transition={{
