40545f16b524997f6001929b947bfc6f10d6b4b6
bossnare <razafimangagervaischristo@gmail.com>
2025-10-20 16:57:29 +0300

    server: formatting...

--- apps/server/src/auth/auth.service.ts
+++ apps/server/src/auth/auth.service.ts
@@ -27,7 +27,7 @@ const transporter: Transporter = nodemailer.createTransport({
   service: "gmail",
   host: "smtp.gmail.com",
   port: 587,
-  secure: false, // true for 465, false for other ports
+  secure: false, // true for 465, false for other ports.
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
