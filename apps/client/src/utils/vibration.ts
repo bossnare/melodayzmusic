b870d1afd938aa7ecff0d0bcac5e824c48d8a0f3
Christo Razafimanga <144788145+bossnare@users.noreply.github.com>
2025-10-21 23:59:43 +0300

    Add 'subtle' vibration type and adjust timings

--- apps/client/src/utils/vibration.ts
+++ apps/client/src/utils/vibration.ts
@@ -1,9 +1,12 @@
-export function vibrate(type: 'low' | 'soft' | 'medium' | 'strong') {
+export function vibrate(type: 'low' | 'subtle' | 'soft' | 'medium' | 'strong') {
   if (!('vibrate' in navigator)) return;
 
   switch (type) {
     case 'low':
-      navigator.vibrate(30);
+      navigator.vibrate(25);
+      break;
+    case 'subtle':
+      navigator.vibrate(15, 5, 10);
       break;
     case 'soft':
       navigator.vibrate(100);
