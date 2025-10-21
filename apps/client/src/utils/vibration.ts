10d50ffd0b1c20ed2d6d6861e395afc1c3ce4d78
Christo Razafimanga <144788145+bossnare@users.noreply.github.com>
2025-10-21 22:46:34 +0300

    Increase vibration duration for 'low' type

--- apps/client/src/utils/vibration.ts
+++ apps/client/src/utils/vibration.ts
@@ -3,7 +3,7 @@ export function vibrate(type: 'low' | 'soft' | 'medium' | 'strong') {
 
   switch (type) {
     case 'low':
-      navigator.vibrate(20);
+      navigator.vibrate(40);
       break;
     case 'soft':
       navigator.vibrate(100);
