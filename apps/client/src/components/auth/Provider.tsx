23c588eda5c77d39253262b42cb229285e663474
Christo Razafimanga <144788145+bossnare@users.noreply.github.com>
2025-10-24 01:06:17 +0300

    Update Provider.tsx: add active interactivity on dark mode

--- apps/client/src/components/auth/Provider.tsx
+++ apps/client/src/components/auth/Provider.tsx
@@ -10,7 +10,7 @@ export function Provider({ size = 'lg' }: { size?: 'lg' | 'xl' }) {
           <Button
             className="w-full rounded-full !bg-secondary-2 !text-secondary-2-foreground 
             dark:text-secondary-foreground hover:!bg-secondary-2/80 dark:!bg-secondary 
-            dark:hover:!bg-secondary/80 hover:!text-secondary-2-foreground/80 dark:hover:!text-secondary-foreground/80 
+            dark:hover:!bg-secondary/80 hover:!text-secondary-2-foreground/80 dark:active:brightness-110 dark:hover:!text-secondary-foreground/80 
             md:w-auto"
             size={size}
           >
