b69dc18ae040aa81df200100f009e47a10f4689e
Christo Razafimanga <144788145+bossnare@users.noreply.github.com>
2025-10-24 07:01:13 +0000

    client:...

--- apps/client/src/components/auth/OtpOverlay.tsx
+++ apps/client/src/components/auth/OtpOverlay.tsx
@@ -96,8 +96,7 @@ function OtpOverlay({
               variant="ghost"
               className="h-auto p-0 hover:text-inherit text-muted-foreground"
             >
-              {isLoading && <Spinner className="size-4" />} Ignorer pour
-              l&apos;instant
+              {isLoading ? <Spinner className="size-4" /> : "Ignorer pour l'instant"} 
             </Button>{' '}
           </div>
           <div className="flex flex-col items-center gap-2 text-sm md:flex-row md:gap-1">
