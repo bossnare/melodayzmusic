751d323ce3fdf548c0de83efbdb70993119a767b
Christo Razafimanga <144788145+bossnare@users.noreply.github.com>
2025-10-25 23:19:53 +0300

    Update AlbumStream.tsx: dragFree mode ON

--- apps/client/src/components/songs/ui/AlbumStream.tsx
+++ apps/client/src/components/songs/ui/AlbumStream.tsx
@@ -5,7 +5,7 @@ import { useEmblaProgress } from '@/hooks/useEmblaProgress';
 
 const AlbumStream = ({ children }: AlbumProps) => {
   const { emblaRef, showFadeStart, showFadeEnd } = useEmblaProgress(
-    false,
+    true,
     1,
     0.25,
     'start',
