9dc547ffe005fccc460d481ccaf576a7a928d9c7
Christo Razafimanga <144788145+bossnare@users.noreply.github.com>
2025-10-26 08:12:43 +0000

    client:fix: gradient on Card

--- apps/client/src/schemas/login.ts
+++ apps/client/src/schemas/login.ts
@@ -1,22 +1,23 @@
-import { EMAIL_REGEX, USERNAME_REGEX } from '@/lib/validators/regex';
+import { EMAIL_REGEX } from '@/lib/validators/regex';
 import { z } from 'zod';
 
 export const loginSchema = z.object({
   email: z
     .string()
     .refine((val) => val.trim() !== '', { message: '' })
-    .refine(
-      (data) => {
-        const value = data.trim();
-        const looksLikeEmail = EMAIL_REGEX.test(value);
-        const looksLikeUsername = USERNAME_REGEX.test(value);
+    .regex(EMAIL_REGEX, { message: 'Email invalide.' }),
+  // .refine(
+  //   (data) => {
+  //     const value = data.trim();
+  //     const looksLikeEmail = EMAIL_REGEX.test(value);
+  //     const looksLikeUsername = USERNAME_REGEX.test(value);
 
-        return looksLikeEmail || looksLikeUsername;
-      },
-      {
-        message: "Email ou nom d'utilisateur(@username) invalide.",
-      }
-    ),
+  //     return looksLikeEmail || looksLikeUsername;
+  //   },
+  //   {
+  //     message: "Email ou nom d'utilisateur(@username) invalide.",
+  //   }
+  // ),
   password: z
     .string()
     .refine((val) => val.trim() !== '', { message: '' })
