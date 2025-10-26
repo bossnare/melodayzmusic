9c33f11c6eadcc1b26f187953ef6670e2635a9d0
Christo Razafimanga <144788145+bossnare@users.noreply.github.com>
2025-10-27 01:05:17 +0300

    Update Player.tsx: change fallback image

--- apps/client/src/components/songs/ui/Player.tsx
+++ apps/client/src/components/songs/ui/Player.tsx
@@ -45,7 +45,7 @@ const Player = () => {
         <div className="w-full overflow-hidden rounded-sm md:w-[30%] bg-gradient-to-tr from-muted/20 to-muted/80 border-muted-foreground/20">
           <Image
             ref={imgRef}
-            src="/img/b2.jpg"
+            src="/img/b3.webp"
             alt="fallback_cover"
             className="object-cover"
             width={1000}
