92d75d1472531a9a0dfced3292998e54988b3c06
bossnare <razafimangagervaischristo@gmail.com>
2025-10-27 10:24:08 +0300

    client:fix: overlay Player

--- apps/client/src/components/songs/ui/Player.tsx
+++ apps/client/src/components/songs/ui/Player.tsx
@@ -52,7 +52,7 @@ const Player = () => {
         <div className="w-full overflow-hidden rounded-sm md:w-[30%] bg-linear-to-tr from-muted/20 to-muted/80 border-muted-foreground/20">
           <Image
             ref={imgRef}
-            src="/img/b4.jpg"
+            src="/img/b6.webp"
             alt="fallback_cover"
             className="object-cover"
             width={1000}
