33b5528bad7b3594374e747021ca630a863cace7
Christo Razafimanga <144788145+bossnare@users.noreply.github.com>
2025-10-27 07:51:06 +0000

    client:UI: change rounded size on card skeleton to compatible on real card

--- apps/client/src/components/skeleton/SongCardSkeleton.tsx
+++ apps/client/src/components/skeleton/SongCardSkeleton.tsx
@@ -6,7 +6,7 @@ export const SongCardSkeleton = () => {
     <>
       <div className="flex flex-col space-y-4">
         <AspectRatio ratio={1}>
-          <Skeleton className="rounded-[2px] size-full bg-foreground/30" />
+          <Skeleton className="rounded-[6.5px] size-full bg-foreground/30" />
         </AspectRatio>
         <div className="space-y-2">
           <Skeleton className="h-4 bg-foreground/30 w-[90%]" />
