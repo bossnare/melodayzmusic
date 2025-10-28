22de9b066b2c99f4a02810de8894446a61db95e6
bossnare <razafimangagervaischristo@gmail.com>
2025-10-28 17:56:55 +0300

    client:ux: add motion on song card

--- apps/client/src/components/motions/wrapper/wrapper.tsx
+++ apps/client/src/components/motions/wrapper/wrapper.tsx
@@ -1,21 +1,31 @@
-import {motion} from 'motion/react'
-import type {BaseProps} from '@/types/base.interface'
+import type { BaseProps } from '@/types/base.interface';
+import { motion } from 'motion/react';
 
-export const Wrapper = ({children}: BaseProps) => {
+const Wrapper = ({ children }: BaseProps) => {
   return (
     <motion.div
-                  initial={{ y: 50, opacity: 0 }}
-                  animate={{ y: 0, opacity: 1 }}
-                  exit={{ y: -50, opacity: 0 }}
-                  transition={{
-                    type: 'spring',
-                    stiffness: 500,
-                    damping: 50,
-                    mass: 1.2,
-                  }}
-                  className="space-y-4">
-                    {children}
-      
+      initial={{ y: 50, opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
+      exit={{ y: -50, opacity: 0 }}
+      transition={{
+        type: 'spring',
+        stiffness: 500,
+        damping: 50,
+        mass: 1.2,
+      }}
+      className="space-y-4"
+    >
+      {children}
     </motion.div>
-  )
-}
+  );
+};
+
+const WhileTapMotion = ({ children }: BaseProps) => {
+  return (
+    <motion.div whileHover={{ scale: 1.01 }} whileTap={{ scale: 0.95 }}>
+      {children}
+    </motion.div>
+  );
+};
+
+export { WhileTapMotion, Wrapper };
--- apps/client/src/components/navigation/Sidebar.tsx
+++ apps/client/src/components/navigation/Sidebar.tsx
@@ -158,7 +158,7 @@ export const Sidebar = ({
                       Non
                     </AlertDialogCancel>
                     <AlertDialogAction
-                      className="font-montserrat h-12 lg:h-auto bg-secondary text-secondary-foreground"
+                      className="font-montserrat h-12 lg:h-auto bg-secondary font-semibold text-secondary-foreground"
                       onClick={logout}
                     >
                       Oui, quitter
