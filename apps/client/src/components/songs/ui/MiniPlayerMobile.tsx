5d97f9bd11b6398542ee8c6d7d1cd401af8a127a
bossnare <razafimangagervaischristo@gmail.com>
2025-10-29 11:06:19 +0300

    client:ux: add select-none for text of mini player

--- apps/client/src/components/songs/ui/MiniPlayerMobile.tsx
+++ apps/client/src/components/songs/ui/MiniPlayerMobile.tsx
@@ -75,7 +75,7 @@ const MiniPlayerMobile = () => {
 
             <div
               onClick={() => handleWait(setTrue)}
-              className="grow cursor-pointer flex items-center gap-2 active:brightness-110"
+              className="grow cursor-pointer select-none flex items-center gap-2 active:brightness-110"
             >
               <div className="rounded-[5px] overflow-hidden size-12 shadow-sm bg-linear-to-tr from-muted/20 via-muted to-muted/40 border-muted-foreground/20">
                 <Image
@@ -89,7 +89,7 @@ const MiniPlayerMobile = () => {
               {/* info */}
               <div
                 style={{ color: `${secondaryColor}` }}
-                className="flex flex-col select-none items-start justify-center gap-0.5"
+                className="flex flex-col items-start justify-center gap-0.5"
               >
                 <p className="text-sm font-bold truncate line-clamp-1">
                   {currentSong?.title}
