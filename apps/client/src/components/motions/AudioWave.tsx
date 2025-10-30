ec4b88ecf9d2a8b48b36fe574be1c17affaff0c3
bossnare <razafimangagervaischristo@gmail.com>
2025-10-30 23:05:22 +0300

    client:fix: bg color for audio wave

--- apps/client/src/components/motions/AudioWave.tsx
+++ apps/client/src/components/motions/AudioWave.tsx
@@ -10,11 +10,12 @@ export default function AudioWave({
       {[...Array(4)].map((_, i) => (
         <span
           key={i}
-          className={`w-1 transition-all! duration-500! bg-[${color}] ${
+          className={`w-1 transition-all! duration-500! ${
             active ? 'animate-wave' : 'h-1'
           }`}
           style={{
             animationDelay: `${i * 0.15}s`,
+            backgroundColor: `${color}`,
           }}
         />
       ))}
