e361f9e152095e587b5b4627aa85bb44531cadb6
bossnare <razafimangagervaischristo@gmail.com>
2025-11-06 17:30:13 +0300

    client:ux: fix play state on overlay player drawer

--- apps/client/src/components/songs/ui/player-drawer.tsx
+++ apps/client/src/components/songs/ui/player-drawer.tsx
@@ -69,7 +69,7 @@ export function PlayerDrawer({ className }: { className?: string }) {
                   <Button
                     variant="ghost"
                     size="xl"
-                    className="w-full transition-none justify-start rounded-xs hover:text-inherit! hover:bg-transparent!
+                    className="w-full transition-none pl-1! justify-start rounded-xs hover:text-inherit! hover:bg-transparent!
                   active:bg-muted-foreground/20! lg:hover:bg-muted-foreground/20! lg:active:opacity-80"
                   >
                     <label.icon className="size-7 md:size-6" /> {label.label}
