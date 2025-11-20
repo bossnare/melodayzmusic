88cdc0638f044cc43202b7725300ba3de252d362
bossnare <razafimangagervaischristo@gmail.com>
2025-11-20 08:22:13 +0100

    client:ux: add dominant color for song cover art to meta theme color

--- apps/client/src/app/services/player-title-sync.tsx
+++ apps/client/src/app/services/player-title-sync.tsx
@@ -1,16 +1,21 @@
+import { usePlayer } from '@/context/playerContext';
 import { useAudioStore } from '@/store/audioStore';
 import { useEffect } from 'react';
 
 export default function PlayerTitleSync() {
   const { currentSong } = useAudioStore();
+  const { dominantColor } = usePlayer();
+  const meta = document.querySelector("meta[name='theme-color']");
 
   useEffect(() => {
     if (currentSong) {
       document.title = `${currentSong.title} - ${currentSong.artist}`;
+      meta?.setAttribute('content', `${dominantColor}`); // browser top bar color
     } else {
       document.title = 'MelodayzMusic - Feel the Beat, Anywhere You Go';
+      meta?.setAttribute('content', `#000`); // browser top bar color if nothing currentSong
     }
-  }, [currentSong]);
+  }, [currentSong, meta, dominantColor]);
 
   return null;
 }
